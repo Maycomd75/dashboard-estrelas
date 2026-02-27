af4d78b345e20fed97f01d3c1227e2ce93e40dc7
Maycom <109564012+Maycomd75@users.noreply.github.com>
2026-02-27 09:23:55 -0300

    Refactor script.js to improve readability and structure

--- script.js
+++ script.js
@@ -121,8 +121,8 @@ async function carregarSupervisores() {
   renderCard("peso_foods", "Peso_Foods", "Meta_Peso_Foods", "perc_Peso_Foods", data);
   renderCard("posit_salty_sup", "Posit_Salty", "Meta_Posit_Salty", "perc_Posit_Salty", data);
   renderCard("posit_foods_sup", "Posit_Foods", "Meta_Posit_Foods", "perc_Posit_Foods", data);
-  renderCard("posit_mix_salty_sup", "Posit_Mix_Salty", "Meta_Posit_Mix_Salty", "perc_Posit_Mix_Salty", data);
-  renderCard("posit_mix_foods_sup", "Posit_Mix_Foods", "Meta_Posit_Mix_Foods", "perc_Posit_Mix_Foods", data);
+  renderCard("posit_mix_salty_sup", "Posit_Mix_Salty", "Meta_Mix_Salty", "perc_Mix_Salty", data);
+  renderCard("posit_mix_foods_sup", "Posit_Mix_Foods", "Meta_Mix_Foods", "perc_Mix_Foods", data);
 }
 
 /* ============================= */
@@ -153,6 +153,7 @@ async function carregarMetas() {
 carregarResumo();
 carregarSupervisores();
 carregarMetas();
+
 
 
 
